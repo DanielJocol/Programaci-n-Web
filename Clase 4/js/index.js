64d3d5c371083f8a981e618953769ed3d2fafbb3
DanielJocol <hjocolm@miumg.edu.gt>
2025-08-08 22:50:35 -0600

    Subida de archivos

---
+++ Clase 4/js/index.js
@@ -0,0 +1,19 @@
+console.log("Hola mundo");
+var variable = "contenido";
+console.log(variable);
+
+let variable2 = "hola de nuevo";
+console.log(variable2);
+
+let texto = document.getElementById("contenido");
+
+console.log(texto)
+
+let etiqueta = document.getElementsByTagName("div");
+
+console.log(etiqueta)
+
+for (let i = 0; i< etiqueta.length;i++)[
+etiqueta[i].style.background = "red"
+
+]
